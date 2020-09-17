32535e170e27468349a09df0c4b97a2f64aa1b20
Peter Vegh <pvegh@posteo.uk>
2020-09-17 17:25:55 +0100

    License

--- setup.py
+++ setup.py
@@ -1,19 +1,28 @@
 import ez_setup
+
 ez_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 
-exec(open('primavera/version.py').read()) # loads __version__
+exec(open("primavera/version.py").read())  # loads __version__
 
 setup(
-    name='primavera',
+    name="primavera",
     version=__version__,
-    author='Zulko',
-    url='https://github.com/Edinburgh-Genome-Foundry/primavera',
-    description='Primer selection + data analysis for DNA assembly validation',
-    long_description=open('pypi-readme.rst').read(),
-    license='see LICENSE.txt',
+    author="Zulko",
+    url="https://github.com/Edinburgh-Genome-Foundry/primavera",
+    description="Primer selection + data analysis for DNA assembly validation",
+    long_description=open("pypi-readme.rst").read(),
+    license="MIT",
     keywords="DNA assembly sequencing primer design",
-    packages=find_packages(exclude='docs'),
-    install_requires=["numpy", "Biopython", "proglog", 'flametree', 'pandas',
-                      'dna_features_viewer', 'dnachisel'])
+    packages=find_packages(exclude="docs"),
+    install_requires=[
+        "numpy",
+        "Biopython",
+        "proglog",
+        "flametree",
+        "pandas",
+        "dna_features_viewer",
+        "dnachisel",
+    ],
+)
